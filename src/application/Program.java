a8ea3dfa12b90c1dfe46842bda89fbd36571dfb5
EduHoff <ehcs.business@gmail.com>
2026-04-28 15:11:20 -0300

    Implementação de lógica para adicionar elemento no meio em uma lista encadeada

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,18 +12,31 @@ public class Program {
         
         System.out.println("Lista vazia (" + lista_ligada.length() + " elementos):");
         lista_ligada.printList();
+        System.out.println();
         
-        for(int i=1; i<= 10; i++) {
+        for(int i=1; i<= 6; i++) {
+        	lista_ligada.addLast(i);
+        }
+
+        for(int i=8; i<= 10; i++) {
         	lista_ligada.addLast(i);
         }
         
         System.out.println("addLast (" + lista_ligada.length() + " elementos):");
         lista_ligada.printList();
+        System.out.println();
         
         lista_ligada.addFirst(0);
         
         System.out.println("addFirst (" + lista_ligada.length() + " elementos):");
         lista_ligada.printList();
+        System.out.println();
+
+        lista_ligada.addMiddle(7, 7);
+
+        System.out.println("addMiddle (" + lista_ligada.length() + " elementos):");
+        lista_ligada.printList();
+        System.out.println();
         
     }
 }
--- src/entities/LinkedList.java
+++ src/entities/LinkedList.java
@@ -7,18 +7,18 @@ public class LinkedList<T> {
 	
 	public void addFirst(T value) {
 		Node<T> new_node  = new Node<>(value);
-		size++;
 		
 		new_node.setNext(head);
 		head = new_node;
+		size++;
 	}
 	
 	public void addLast(T value) {
 		Node<T> new_node = new Node<>(value);
-		size++;
 		
 		if (head == null) {
             head = new_node;
+			size++;
             return;
         }
 		
@@ -28,6 +28,35 @@ public class LinkedList<T> {
 		}
 		
 		aux.setNext(new_node);
+		size++;
+	}
+
+	public void addMiddle(int index, T value) {
+		if(index < 0 || index > size) throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+		
+		if(index == 0) {
+			addFirst(value);
+			return;
+		}
+
+		if(index == size){
+			addLast(value);
+			return;
+		}
+
+		Node<T> new_node = new Node<>(value);
+		
+		Node<T> aux1 = head;
+		int count = 0;
+		while(count != index-1) {
+			aux1 = aux1.getNext();
+			count++;
+		}
+
+		Node<T> aux2 = aux1.getNext();
+		aux1.setNext(new_node);
+		new_node.setNext(aux2);
+		size++;
 	}
 	
 	public void printList() {
